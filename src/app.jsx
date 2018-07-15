8e9048f3f300236b26357d5dadb88fe8e735b720
ZhangZY <346114608@qq.com>
2018-07-15 16:10:05 +0800

    初始化

--- src/app.jsx
+++ src/app.jsx
@@ -7,7 +7,7 @@ import "./index.scss";
 ReactDOM.render(
   <div>
     <i className="fa fa-address-book"></i>
-    <h1>Hello,world!</h1>
+    <h1>Hello,world!!!</h1>
   </div>,
   document.getElementById("app")
 );
