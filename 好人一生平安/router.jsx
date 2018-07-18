0cc4f52dafe547b8884cb89f0e1789ae2126d2b8
ouaisheinie <346114608@qq.com>
2018-07-18 18:23:58 +0800

    router.jsx

--- 好人一生平安/router.jsx
+++ 好人一生平安/router.jsx
@@ -1,3 +1,39 @@
+//页面路由
+window.location.href = 'http://www.baidu.com';
+history.back()
+
+//hash 路由
+window.location = "#hash";
+window.onhashchange  = function() {
+  console.log('current hash:',window.location.hash);
+}
+
+//h5路由 
+//推进一个状态
+history.pushState('name','title','/path');
+//替换一个状态
+history.replaceState('name','title','/path');
+//popstate  在window上监听 popstate 只处理后退 不处理前进
+window.popstate = function(){
+  console.log(window.location.href);
+  console.log(window.location.pathname); //绝对路径
+  console.log(window.location.hash);//hash
+  console.log(window.location.search);//hash
+}
+
+
+
+// hash router
+import {HashRouter as Router,Route,Link} from 'react-router-dom';
+
+//browser router  这个只是前端路由 后端是请求不到的
+import {BrowserRouter as Router,Route,Link} from 'react-router-dom';
+
+
+
+
+/* 课程代码 */
+
 import React, {Component}from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router,Route,Link,Switch} from 'react-router-dom';
