e8119865348b4b76a774fed005f68cad580819a4
ZhangZY <346114608@qq.com>
2018-07-23 22:20:16 +0800

    通用部分的开发

--- src/app.jsx
+++ src/app.jsx
@@ -1,69 +1,31 @@
 import React, {Component}from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router,Route,Link,Switch} from 'react-router-dom';
+import {BrowserRouter as Router,Route,Link,Switch,Redirect} from 'react-router-dom';
+// 页面
+import Home from 'page/home/index.jsx';
+import Layout from "component/layout/index.jsx";
 
-class A extends Component{
+class App extends Component{
   constructor(props) {
     super(props)
+    
   }
   render(){
     return (
-      <div>
-        Component A
-        <Switch>
-          <Route exact path={`${this.props.match.path}`} 
-          render={(route)=>{
-            return <div>当前组建是不带参数的A</div>
-          }}/>
-          <Route exact path={`${this.props.match.path}/sub`} 
-          render={(route)=>{
-            return <div>当前组建是sub</div>
-          }}/>
-          <Route exact path={`${this.props.match.path}/:id`} 
-          render={(route)=>{
-            return <div>当前组建是带参数的A,参数是:{route.match.params.id}</div>
-          }}/>
-        </Switch>
-      </div>
-    )
-  }
-}
-class B extends Component{
-  constructor(props) {
-    super(props)
-  }
-  render(){
-    return (
-      <div>Component B</div>
-    )
-  }
-}
-class Wrapper extends Component{
-  constructor(props) {
-    super(props)
-  }
-  render(){
-    return (
-      <div>
-        <Link to="/a">组件A</Link>
-        <br/>
-        <Link to="/a/123">带参数的组件A</Link>
-        <br/>
-        <Link to="/b">组件B</Link>
-        <br/>
-        <Link to="/a/sub">/a/sub</Link>
-        {this.props.children}
-      </div>
+      <Router>
+        <Layout>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/product" component={Home} />
+            <Route exact path="/product-category" component={Home} />
+          </Switch>
+        </Layout>
+      </Router>
     )
   }
 }
 
 ReactDOM.render(
-  <Router>
-    <Wrapper>
-      <Route path="/a" component={A}/>
-      <Route path="/b" component={B}/>
-    </Wrapper>
-  </Router>,
+  <App />,
   document.getElementById('app')
 )
